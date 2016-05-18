409291ae22ed04cef3bf7930b98352dfabe837c7
Hendrik Siedelmann <hendrik.siedelmann@googlemail.com>
2016-05-18 15:10:09 +0200

    load reference rotation and translation from blender

--- evaluation/eval.cpp
+++ evaluation/eval.cpp
@@ -23,6 +23,22 @@ using namespace cv;
 using namespace std;
 using namespace hdmarker;
 
+Mat read_double_m(string path, int rows, int cols)
+{
+  double m;
+  Mat out = Mat::zeros(rows, cols, CV_64FC1);
+  
+  ifstream ifs(path);
+  int i = 0;
+  while (ifs >> m)
+  {
+    out.at<double>(i / cols, i % cols) = m;
+    i++;
+  }
+  
+  return out;
+}
+
 int main(int argc, char* argv[])
 {  
   /*Matx34d world_to_cam(-0.0000, -1.0000,  0.0000,  10.1065,
@@ -51,7 +67,7 @@ Matx34d world_to_cam(0.94763702, 0.31785029,  -0.03090816, -10.84295086,
                0.0000, 2100.0000, 540.0000,
               0.0000,    0.0000,   1.0000);
                        
-  cout << world_to_cam << "\n";
+  cout << world_to_cam << "\n";  
   
   Matx41d p_w(8.0, 8.0, 0.0, 1.0);
 
@@ -80,6 +96,7 @@ Matx34d world_to_cam(0.94763702, 0.31785029,  -0.03090816, -10.84295086,
   Mat debug;
   Mat debug2;
   Mat dirs;
+  
   hdmarker_detect_subpattern(gray, corners_rough, corners, 2, &unit_size_res, &paint);
   
   imwrite("points.tif", paint);
@@ -93,8 +110,8 @@ Matx34d world_to_cam(0.94763702, 0.31785029,  -0.03090816, -10.84295086,
   
   Point2d diff(0,0);
   
-  std::vector<Point3f> wpoints;
-  std::vector<Point2f> ipoints;
+  std::vector<Point3f> wpoints, wpoints2;
+  std::vector<Point2f> ipoints, ipoints2;
   
   std::vector<std::vector<Point3f>> wpoints_v;
   std::vector<std::vector<Point2f>> ipoints_v;
@@ -129,7 +146,7 @@ Matx34d world_to_cam(0.94763702, 0.31785029,  -0.03090816, -10.84295086,
     circle(debug2, corners[ci].p*16, 3, CV_RGB(255,255,255), -1, CV_AA, 4);
   
     wpoints.push_back(Point3f(corners[ci].id.x*unit_size_res,corners[ci].id.y*unit_size_res, 0));
-    ipoints.push_back(Point2f(corners[ci].p.x,corners[ci].p.y));
+    ipoints.push_back(Point2f(corners[ci].p.x, corners[ci].p.y));
   }
   
   cout << "avg: " << diff*(1.0/corners.size()) << "\n";
@@ -164,11 +181,26 @@ Matx34d world_to_cam(0.94763702, 0.31785029,  -0.03090816, -10.84295086,
   cout << "trans:\n" << trans << "\n";
   
   
+  Mat rot_ref = read_double_m("rot.txt", 3, 3);
+  rot_ref.at<double>(0, 1) *= -1;
+  rot_ref.at<double>(0, 2) *= -1;
+  rot_ref.at<double>(1, 0) *= -1;
+  rot_ref.at<double>(2, 0) *= -1;
+  
+  Mat trans_ref = read_double_m("trans.txt", 3, 1);
+  trans_ref.at<double>(0) *= -1;
+  
+  cout << "rot ref:\n" << rot_ref << "\n";
+  cout << "rot diff:\n" << rot-rot_ref << "\n";
+  cout << "trans ref:\n" << trans_ref << "\n";
+  cout << "trans diff:\n" << trans-trans_ref << "\n";
+  
+  
   //cout << "reverse:\n" << -rot.t()*trans << "\n";
 
   
   clif::Mat_<float> proxy({2, 33, 19});
-  proxy_backwards_pers_poly_generate(proxy, ipoints, wpoints, Point2i(paint.size().width, paint.size().height));
+  proxy_backwards_pers_poly_generate<0,0>(proxy, ipoints, wpoints, Point2i(paint.size().width, paint.size().height),paint.size().width/proxy[1]*0.5, 10);
   
   diff = Point2d(0,0);
   rms = 0;
@@ -197,7 +229,7 @@ Matx34d world_to_cam(0.94763702, 0.31785029,  -0.03090816, -10.84295086,
     //cout << p << "\n";
     //cout << corners[ci].p << " " << d << "\n\n";
     
-    line(dirs, p*16, (p+d*50)*16, CV_RGB(255,255,255), 1, CV_AA, 4);
+    line(dirs, p*16, (p+d*200)*16, CV_RGB(255,255,255), 1, CV_AA, 4);
     
     diff += d;
     
@@ -205,12 +237,26 @@ Matx34d world_to_cam(0.94763702, 0.31785029,  -0.03090816, -10.84295086,
     
     //circle(debug, p*16, 3, CV_RGB(255,255,255), -1, CV_AA, 4);
     //circle(debug2, corners[ci].p*16, 3, CV_RGB(255,255,255), -1, CV_AA, 4);
+    
+    wpoints2.push_back(Point3f(proxy(0, idx[1], idx[2]),proxy(1, idx[1], idx[2]), 0));
+    ipoints2.push_back(fit_p);
   }
   
   imwrite("dirs_proxy.tif", dirs);
   
   cout << "avg: " << diff*(1.0/corners.size()) << "\n";
   cout << "rms: " << sqrt(rms*(1.0/corners.size())) << "\n";
+  
+  
+  solvePnP(wpoints2, ipoints2, cam_to_img, noArray(), rot, trans);
+  
+  Rodrigues(rot, rot);
+  cout << "rotmat:\n" << rot << "\n";
+  cout << "trans:\n" << trans << "\n";
+  
+  
+  cout << "r diff:\n" << rot-rot_ref << "\n";
+  cout << "t diff:\n" << trans - trans_ref << "\n";
   
   return EXIT_SUCCESS;
 }
