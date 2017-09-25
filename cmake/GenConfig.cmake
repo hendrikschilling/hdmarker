b68c4d19fc72f5f071a0d96bcfeb4164f801cf64
mdiebold <maximilian.diebold@iwr.uni-heidelberg.de>
2017-09-25 09:16:23 +0200

    update build cmake files

--- cmake/GenConfig.cmake
+++ cmake/GenConfig.cmake
@@ -3,6 +3,6 @@
 #  ${BIN_DIR}/OpenCVConfig.cmake              -> For use *without* "make install"
 # -------------------------------------------------------------------------------------------
 
-configure_file("${${CMAKE_PROJECT_NAME}_SOURCE_DIR}/cmake/${CMAKE_PROJECT_NAME}Config.cmake.in" "${CMAKE_BINARY_DIR}/${CMAKE_PROJECT_NAME}Config.cmake" @ONLY)
+configure_file("${hdmarker_SOURCE_DIR}/cmake/hdmarkerConfig.cmake.in" "${hdmarker_BINARY_DIR}/hdmarkerConfig.cmake" @ONLY)
 
 
